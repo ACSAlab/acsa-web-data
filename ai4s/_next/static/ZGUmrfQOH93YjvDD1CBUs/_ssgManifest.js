2a70d160af82bec101b8c447a552ee2a9049e48b
litou <litou@ustc.edu.cn>
2026-09-15 16:12:42 +0800

    ai4s: AI4S course site static export (Next.js, basePath=/ai4s)

---
+++ ai4s/_next/static/ZGUmrfQOH93YjvDD1CBUs/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fad\u002Fbaseline\u002Fcase\u002F[id]","\u002Fad\u002Fneurocouncil\u002Fcase\u002F[id]","\u002Ftopics\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
