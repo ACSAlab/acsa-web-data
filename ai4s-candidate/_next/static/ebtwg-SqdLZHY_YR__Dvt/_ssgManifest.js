e4fd4528208bae9308b6ea6f35f4be6a6d234919
litou <litou@ustc.edu.cn>
2026-09-24 11:28:20 +0800

    ai4s prod + ai4s-candidate: three-version deploy (previous/prod/candidate), faculty EN 12px, stats reset

---
+++ ai4s-candidate/_next/static/ebtwg-SqdLZHY_YR__Dvt/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fad\u002Fv1\u002Fcases\u002F[id]","\u002Fad\u002Fv2\u002Fcases\u002F[id]","\u002Felectives\u002F[slug]","\u002Ftopics\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
